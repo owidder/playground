be4fb5f49562e8c7c5e19b09887b05a3aa61a504
Oliver Widder <oliver.widder@gmail.com>
2020-08-01 13:56:50 +0200

    bookmarks (3)

--- src/tf/bookmarks.ts
+++ src/tf/bookmarks.ts
@@ -6,7 +6,12 @@ export type Bookmark = {
 const bookmarks: Bookmark[] = [];
 
 export const addBookmark = (bookmark: Bookmark): void => {
-    bookmarks.push(bookmark);
+    const index = bookmarks.findIndex(b => b.url == bookmark.url);
+    if(index < 0) {
+        bookmarks.push(bookmark);
+    } else {
+        bookmarks.splice(index, 1, bookmark);
+    }
 }
 
 export const deleteBookmark = (name: string): void => {
